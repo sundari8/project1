988b4e86a95aebce3d9b988760b924f630574811
Sundari <sundarig07032004@gmail.com>
2023-07-21 14:24:00 +0530

    test

---
+++ src/join.java
@@ -0,0 +1,10 @@
+public class join {
+    String name;
+    String dept;
+
+    public void printer() {
+        System.out.println("Username :"+ name);
+        System.out.println("Username :"+ dept);
+    }
+
+}
---
+++ src/second.java
@@ -0,0 +1,14 @@
+public class second {
+    public static void main(String[] args) {
+        join s1 = new join();
+        s1.name = "Susi";
+        s1.dept = "ECE";
+        join s2 = new join();
+        s2.name = "Sundari";
+        s2.dept = "ECE";
+
+        s1.printer();
+        s2.printer();
+    }
+}
+
